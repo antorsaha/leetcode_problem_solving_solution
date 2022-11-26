0f4abba3136bea0b90ffe52d14ecd2c1449608ef
Antor Saha <antor145648s@gmail.com>
2022-11-26 14:33:49 +0600

    update P0986_Interval_List_Intersections

--- src/solutions/P0986_Interval_List_Intersections.java
+++ src/solutions/P0986_Interval_List_Intersections.java
@@ -1,25 +1,30 @@
 package solutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class P0986_Interval_List_Intersections {
     class Solution {
-        public int maxArea(int[] height) {
-            int i=0;
-            int j = height.length-1;
-            int ans =0;
+        public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+            if (firstList.length == 0 || secondList.length == 0)
+                return new int[0][0];
+            int firstIndex = 0;
+            int secondIndex = 0;
+            int start = 0;
+            int end = 0;
+            List<int[]> result = new ArrayList<>();
 
-            while(i < j ){
-                int breadth = j-i;
-                int length = Math.min(height[j],height[i]);
+            while (firstIndex < firstList.length && secondIndex < secondList.length) {
+                start = Math.max(firstList[firstIndex][0], secondList[secondIndex][0]);
+                end = Math.min(firstList[firstIndex][1], secondList[secondIndex][1]);
 
-                ans = Math.max(ans,breadth*length);
+                if (end >= start)
+                    result.add(new int[]{start, end});
 
-                if(height[i]<=height[j]){
-                    i++;
-                }else{
-                    j--;
-                }
+                if (end == firstList[firstIndex][1]) firstIndex++;
+                if (end == secondList[secondIndex][1]) secondIndex++;
             }
-            return ans;
+            return result.toArray(new int[result.size()][2]);
         }
     }
 }
