a280d39dd37ee3588006b2eaa6191ac4d2444f18
Antor Saha <antor145648s@gmail.com>
2023-01-05 12:49:51 +0600

    add P0797_All_Paths_From_Source_to_Target

---
+++ src/solutions/P0797_All_Paths_From_Source_to_Target.java
@@ -0,0 +1,32 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P0797_All_Paths_From_Source_to_Target {
+    class Solution {
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> temp = new ArrayList<>();
+            temp.add(0);
+            dfs(graph, ans, temp, 0);
+            return ans;
+        }
+
+        public void dfs(int[][] graph, List<List<Integer>> ans, List<Integer> temp, int start) {
+
+            if (start == graph.length - 1) {
+                ans.add(new ArrayList<>(temp));
+                return;
+            }
+
+            for (int i : graph[start]) {
+                temp.add(i);
+                dfs(graph, ans, temp, i);
+                temp.remove(temp.size() - 1);
+            }
+
+        }
+    }
+}
