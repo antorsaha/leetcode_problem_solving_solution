e6e8e75c63d705562f546c3f34dac464626e74a6
Antor Saha <antor145648s@gmail.com>
2023-02-02 00:22:09 +0600

    add P111_Minimum_Depth_of_Binary_Tree

---
+++ src/solutions/P111_Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,34 @@
+package solutions;
+
+import utrils.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P111_Minimum_Depth_of_Binary_Tree {
+    class Solution {
+        public int minDepth(TreeNode root) {
+            Queue<TreeNode> q = new LinkedList<>();
+            if (root == null)
+                return 0;
+            int minDepth = 0;
+            q.add(root);
+            while (!q.isEmpty()) {
+                int size = q.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = q.poll();
+                    if (node.left == null && node.right == null)
+                        return minDepth + 1;
+                    if (node.left != null)
+                        q.add(node.left);
+                    if (node.right != null)
+                        q.add(node.right);
+                }
+                minDepth++;
+
+            }
+            return minDepth;
+        }
+
+    }
+}
