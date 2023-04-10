d2210e5afd52327f76bf6c7d51077669661cd037
Antor Saha <antor145648s@gmail.com>
2023-04-10 20:46:35 +0600

    add P0056_Merge_Intervals

---
+++ src/solutions/P0133_Clone_Graph.java
@@ -0,0 +1,33 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P0133_Clone_Graph {
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+
+    class Solution {
+        public Node cloneGraph(Node node) {
+
+        }
+    }
+}
