cc48a44393fa0af3ecf6e536fca526b0a9036b71
antorsaha <antor145648s@gmail.com>
2023-09-23 01:17:10 +0600

    add P392_Is_Subsequence

---
+++ src/solutions/P392_Is_Subsequence.java
@@ -0,0 +1,20 @@
+package solutions;
+
+import java.util.HashMap;
+
+public class P392_Is_Subsequence {
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+            int i = 0, j = 0;
+            while(i < s.length() && j < t.length()) {
+                if(s.charAt(i) == t.charAt(j)) {
+                    i+=1;
+                    j+=1;
+                } else {
+                    j+=1;
+                }
+            }
+            return (i == s.length())?true:false;
+        }
+    }
+}
