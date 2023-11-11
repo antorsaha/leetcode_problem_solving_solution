2cb218269ef9354a3eb92e066b65c1f862c64ee1
antorsaha <antor145648s@gmail.com>
2023-11-11 17:44:29 +0600

    add P2259_Remove_Digit_From_Number_to_Maximize_Result

---
+++ src/solutions/P2259_Remove_Digit_From_Number_to_Maximize_Result.java
@@ -0,0 +1,17 @@
+package solutions;
+
+public class P2259_Remove_Digit_From_Number_to_Maximize_Result {
+    class Solution {
+        public String removeDigit(String number, char digit) {
+            int index = 0,n=number.length();
+            for(int i=0;i<n;i++){
+                if(number.charAt(i)==digit){
+                    index = i;
+                    if(i<n-1 && digit < number.charAt(i+1)) break;
+                }
+            }
+            number = number.substring(0, index) + number.substring(index+1);
+            return number;
+        }
+    }
+}
