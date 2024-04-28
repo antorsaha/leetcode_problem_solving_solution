ce08ea8c4d63a4f0cfc1997733f9acc8c9d5ebca
antorsaha <antor145648s@gmail.com>
2024-04-28 21:25:54 +0600

    updated P0242_Valid_Anagram

--- src/solutions/P0242_Valid_Anagram.java
+++ src/solutions/P0242_Valid_Anagram.java
@@ -5,32 +5,19 @@ import java.util.HashMap;
 public class P0242_Valid_Anagram {
     class Solution {
         public boolean isAnagram(String s, String t) {
-            HashMap<Character, Integer> sMap = new HashMap<>();
+            int arr[] = new int[26];
 
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (sMap.containsKey(c)) {
-                    sMap.put(c, sMap.get(c) + 1);
-                } else {
-                    sMap.put(c, 1);
-                }
-            }
+            for(char x : s.toCharArray())
+                arr[x-'a']++;
+            for(char x : t.toCharArray())
+                arr[x-'a']--;
 
-            for (int i = 0; i < t.length(); i++) {
-                char c = t.charAt(i);
-
-                int count = sMap.getOrDefault(c, 0);
-
-                if (count == 0) {
+            for(int val: arr)
+            {
+                if(val!=0)
                     return false;
-                } else if (count == 1) {
-                    sMap.remove(c);
-                } else {
-                    sMap.put(c, count - 1);
-                }
             }
-
-            return sMap.keySet().size() == 0;
+            return true;
 
         }
     }
